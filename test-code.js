90338aee33bf1c08196adbefda13e433c4a770d6
Server <november14giel@gmail.com>
2018-02-24 14:26:49 -0500

    mongo-starter

--- test-code.js
+++ test-code.js
@@ -1,78 +1,55 @@
-const password = function (pin) {
-    const testingDigits =  /\d/.test(pin);//digits
-    const not_a_Digit = (/\D/i).test(pin); //alphabet
-    
-    //console check
-    console.log(not_a_Digit);
-    console.log(pin);
-    console.log("pin length", pin.length);
-    if ((pin.length == 4) && (not_a_Digit == false)) {
-        console.log("good not less than 4, not over 6, and no numbers");
-        return true;
-    } else if ((pin.length == 6) && (not_a_Digit == false)) {
-        console.log("good not less than 4, not over 6, and no numbers");
-        return true;
-    } else {
-        console.log("check again...");
-        return false;
-    }
-};
 
-password("123!4");
-
-
-
-// //==================
-// $(document).ready(function () {
-//     //testing for connection to html file
-//     console.log("nice, js file is linked.");
-//     //on click event
-//     $("#btn").on("click", function(event){
-//         //event.stopPropagation();//stops parent listeners from firing or `bubbling`
-//         //event.preventDefault();//prevents default action to ensue, (e.g ink, submit etc...)
+//==================
+$(document).ready(function () {
+    //testing for connection to html file
+    console.log("nice, js file is linked.");
+    //on click event
+    $("#btn").on("click", function(event){
+        //event.stopPropagation();//stops parent listeners from firing or `bubbling`
+        //event.preventDefault();//prevents default action to ensue, (e.g ink, submit etc...)
         
-//         password();
-//         return false; //also works as a `preventDefault`/`stopPropagation` in this situation 
-//     })
+        password();
+        return false; //also works as a `preventDefault`/`stopPropagation` in this situation 
+    })
 
 
-//     const password = function() {
-//         // const lowerCasePin = pin.toLowerCase();
-//         // const pinArr = lowerCasePin.split();
-//         const pw = $("#passWord").val().trim();
-//         const pin = pw;
-//         const regex = /\d/g;
+    const password = function() {
+        // const lowerCasePin = pin.toLowerCase();
+        // const pinArr = lowerCasePin.split();
+        const pw = $("#passWord").val().trim();
+        const pin = pw;
+        const regex = /\d/g;
 
-//         console.log(pin);
-//         console.log("pin length", pin.length)
+        console.log(pin);
+        console.log("pin length", pin.length)
 
-//         function hasNumbers(t) {
+        function hasNumbers(t) {
             
-//             return regex.test(t);
-//         }    
+            return regex.test(t);
+        }    
 
         
-//         if ((pin.length >= 4) && (pin.length <= 6)) {
-//             console.log("good not less than 4, not over 6");
-//             alert("good not less than 4, not over 6");
-//             return true;
-//         } else if (pin.length > 6) {
-//             console.log("too much");
-//             alert("too much");
-//             return false;
-//         } else if (pin.length < 3) {
-//             console.log("a little more please");
-//             alert("a little more please");
-//             return false;
-//         } else if (pin.indexOf(/^[a-zA-Z]*$/) > -1) {
-//             console.log("only digits please");
-//             alert("only digits please");
-//             return false;
-//         }
+        if ((pin.length >= 4) && (pin.length <= 6)) {
+            console.log("good not less than 4, not over 6");
+            alert("good not less than 4, not over 6");
+            return true;
+        } else if (pin.length > 6) {
+            console.log("too much");
+            alert("too much");
+            return false;
+        } else if (pin.length < 3) {
+            console.log("a little more please");
+            alert("a little more please");
+            return false;
+        } else if (pin.indexOf(/^[a-zA-Z]*$/) > -1) {
+            console.log("only digits please");
+            alert("only digits please");
+            return false;
+        }
 
       
-//             $("#passWord").val("");   
-//     }
-// });
+            $("#passWord").val("");   
+    }
+});
 
 
--- testing-code/instanceInArray.js
+++ testing-code/instanceInArray.js
@@ -1,23 +1,20 @@
-function count() {
-    array_elements = ["a", "b", "c", "d", "e", "a", "b", "c", "f", "g", "h", "h", "h", "e", "a"];
+// Here we will count the number of instance
+// a value appears inside an array:
+var arr = [5, 5, 5, 2, 2, 2, 2, 2, 9, 4];
+var counts = {};//inside the for-loop, 
+                //`counts` automatically counts number of instance 
+                //that a value inside an array occured. 
 
-    array_elements.sort();
-
-    var current = null;
-    var cnt = 0;
-    for (var i = 0; i < array_elements.length; i++) {
-        if (array_elements[i] != current) {
-            if (cnt > 0) {
-                document.write(current + ' comes --> ' + cnt + ' times<br>');
-            }
-            current = array_elements[i];
-            cnt = 1;
-        } else {
-            cnt++;
-        }
-    }
-    if (cnt > 0) {
-        document.write(current + ' comes --> ' + cnt + ' times');
-    }
-
+for (var i = 0; i < arr.length; i++) {
+    var num = arr[i]; console.log(num);
+    counts[num] = counts[num] ? counts[num] + 1 : 1;
+    console.log(counts[num])
 }
+
+console.log(counts[5], counts[2], counts[9], counts[4]);
+
+
+// ====NOTES ON TERNARY OPERATOR====
+// `?` operator will be used
+// example: var voteable = (age < 18) ? "Too young" : "Old enough"; >>> means
+// If the variable age is a value below 18, the value of the variable voteable will be "Too young", otherwise the value of voteable will be "Old enough".
