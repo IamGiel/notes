f0dbed86958cb6a69f5cf8252deb0e666a3faf21
Server <november14giel@gmail.com>
2018-04-06 14:12:41 -0400

    dirList instead of

--- Angular/directory.js
+++ Angular/directory.js
@@ -1,37 +1,39 @@
 angular
   .module("directoryApp", [])
-  .controller("directoryController", function($scope) {
-    $scope.userData = [
-      // {
-      //   firstName: "Gel",
-      //   skill: "sword",
-      //   email: "gel@mailer.com"
-      // },
-      // {
-      //   firstName: "Mike",
-      //   skill: "speed",
-      //   email: "mike@mailer.com"
-      // },
-      // {
-      //   firstName: "Jen",
-      //   skill: "intel",
-      //   email: "jen@mailer.com"
-      // },
-      // {
-      //   firstName: "Love",
-      //   skill: "anti-gravity",
-      //   email: "love@mailer.com"
-      // }
+  .controller("directoryController", function() {
+
+    var dirList = this;
+    dirList.userData = [
+      {
+        firstName: "Gel",
+        skill: "sword",
+        email: "gel@mailer.com"
+      },
+      {
+        firstName: "Mike",
+        skill: "speed",
+        email: "mike@mailer.com"
+      },
+      {
+        firstName: "Jen",
+        skill: "intel",
+        email: "jen@mailer.com"
+      },
+      {
+        firstName: "Love",
+        skill: "anti-gravity",
+        email: "love@mailer.com"
+      }
     ];
 
-    $scope.addPerson = function() {
-      $scope.userData.push({
-        firstName:$scope.firstName,
-        skill:$scope.skill,
-        email:$scope.email
+    dirList.addPerson = function() {
+      dirList.userData.push({
+        firstName:dirList.firstName,
+        skill:dirList.skill,
+        email:dirList.email
       });
-      $scope.firstName = "";
-      $scope.skill = "";
-      $scope.email = "";
+      dirList.firstName = "";
+      dirList.skill = "";
+      dirList.email = "";
     };
   });
