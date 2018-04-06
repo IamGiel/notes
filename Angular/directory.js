191ca6b7af89717af9e21c4f2d5edf2327b58f72
Server <november14giel@gmail.com>
2018-04-06 14:34:22 -0400

    ng-class implementation with filters

--- Angular/directory.js
+++ Angular/directory.js
@@ -7,21 +7,25 @@ angular
       {
         firstName: "Gel",
         skill: "sword",
+        hitPoints: 12,
         email: "gel@mailer.com"
       },
       {
         firstName: "Mike",
         skill: "speed",
+        hitPoints: 14,
         email: "mike@mailer.com"
       },
       {
         firstName: "Jen",
         skill: "intel",
+        hitPoints: 13,
         email: "jen@mailer.com"
       },
       {
         firstName: "Love",
         skill: "anti-gravity",
+        hitPoints: 10,
         email: "love@mailer.com"
       }
     ];
@@ -30,10 +34,12 @@ angular
       dirList.userData.push({
         firstName:dirList.firstName,
         skill:dirList.skill,
+        hitPoints:dirList.hitPoints,
         email:dirList.email
       });
       dirList.firstName = "";
       dirList.skill = "";
+      dirList.hitPoints = "";
       dirList.email = "";
     };
   });
