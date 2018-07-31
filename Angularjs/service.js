6044f3b7f86775963a06254f17f75877ae3f1595
IamGiel <lfagel84@gmail.com>
2018-07-31 12:11:40 -0400

    adding service to controiller

--- Angularjs/service.js
+++ Angularjs/service.js
@@ -50,3 +50,63 @@ app.service('myService', function ($http, $q) {
         return _finalUrl;
     }
 });
+
+// Now attach all the method using `this` keyword
+
+app.service('myService', function ($http, $q) {
+    var baseUrl = 'https://itunes.apple.com/search?term=';
+    var _artist = '';
+    var _finalUrl = '';
+
+    var makeUrl = function () {
+        _artist = _artist.split(' ').join('+');
+        _finalUrl = baseUrl + _artist + '&callback=JSON_CALLBACK';
+        return _finalUrl;
+    }
+
+    this.setArtist = function (artist) { //see here <<
+        _artist = artist;
+    }
+
+    this.getArtist = function () { //see here <<
+        return _artist;
+    }
+
+    this.callItunes = function () { //see here <<
+        makeUrl()
+        var deferred = $q.defer();
+        $http({
+            method: 'JSONP',
+            url: _finalUrl
+        }).success(function (data) {
+            deferred.resolve(data);
+        }).error(function () {
+            deferred.reject('There was an error');
+        })
+
+        return deferred.promise;
+    }
+
+});
+
+// **** Now implement in our controller ****
+
+// Now just like in our factory, setArtist, getArtist, 
+// and callItunes will be available in whichever controller we pass myService into.
+// Here’ s the myService controller(which is almost exactly the same as our factory controller).
+
+app.controller('myService', function ($scope, myService) {
+    $scope.data = {};
+    $scope.updateArtist = function () {
+        myService.setArtist($scope.data.artist);
+    }
+
+    $scope.submitArtist = function () {
+        myService.callItunes()
+            .then(function (data) {
+                $scope.data.artistData = data;
+            }, function (data) {
+                alert(data);
+            })
+    }
+});
