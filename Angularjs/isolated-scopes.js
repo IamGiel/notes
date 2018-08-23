dfa161824035c388ff86a602548a7972c74743ba
IamGiel <november14giel@gmail.com>
2018-08-23 15:17:55 -0400

    prevent ng-include from creating isolated scopes

---
+++ Angularjs/isolated-scopes.js
@@ -0,0 +1,9 @@
+// Prevent `ng-include`
+// from creating isolated scope.AngularJS
+
+//SOLUTION:
+// create a your own directive that can load specified template withoud isolated scope.
+
+// Example Here: 
+
+// https: //stackoverflow.com/questions/29115207/prevent-ng-include-from-creating-isolated-scope-angularjs/29115532#29115532
