0f30b061275a53f8908697cd47175432b34bdca8
IamGiel <november14giel@gmail.com>
2018-10-07 15:03:01 -0400

    added questions encountered in bit bucket private repository projects

--- Angular/databinding1/src/app/app.module.ts
+++ Angular/databinding1/src/app/app.module.ts
@@ -7,13 +7,17 @@ import { AppComponent } from './app.component';
 import { GameControllerComponent } from './game-controller/game-controller.component';
 import { OddComponent } from './odd/odd.component';
 import { EvenComponent } from './even/even.component';
+import { UploadListComponent } from './uploads/upload-list/upload-list.component';
+import { UploadFormComponent } from './uploads/upload-form/upload-form.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     GameControllerComponent,
     OddComponent,
-    EvenComponent
+    EvenComponent,
+    UploadListComponent,
+    UploadFormComponent
   ],
   imports: [
     BrowserModule,
